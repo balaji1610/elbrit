c958803758f07ebe1942cfac82ab58b21477c1d6
Balaji <81224567+balaji1610@users.noreply.github.com>
2024-12-18 23:54:02 +0530

    Added Redesign Flex

--- src/app/components/listFeature.tsx
+++ src/app/components/listFeature.tsx
@@ -8,13 +8,11 @@ import Feature4 from "../../../public/feature04.png";
 import Feature5 from "../../../public/feature05.png";
 import Feature6 from "../../../public/feature06.png";
 import Image, { StaticImageData } from "next/image";
+import useMediaQuery from "@mui/material/useMediaQuery";
 import Content from "../utils/content.json";
 import Typography from "@mui/material/Typography";
 export default function ListFeature() {
   const { Features } = Content;
-
-  const FeaturePartOne = Features.slice(0, 3);
-  const FeaturePartTwo = Features.slice(3, 6);
 
   const listFeatureImage: { [key: string]: StaticImageData } = {
     "clinically studied": Feature1,
@@ -24,98 +22,62 @@ export default function ListFeature() {
     "No Risk": Feature5,
     "GMO free": Feature6,
   };
+  const isDeskopScreen = useMediaQuery("(min-width:600px)");
   return (
     <>
-      <Grid container>
-        <Grid size={{ md: 2, sm: 2, xs: 12 }}></Grid>
-
-        {FeaturePartOne.map((el) => {
-          const { Title, DescriptionOne, DescriptionTwo } = el;
-          return (
-            <Grid
-              size={{ md: 3, sm: 3, xs: 12 }}
-              sx={{
-                backgroundColor: "#103f4e",
-                color: "#fff",
-              }}
-            >
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "row",
-                  justifyContent: "center",
-                  padding: "20px",
-                }}
-              >
-                {" "}
-                <Stack direction="column">
-                  <Box>
-                    <Image
-                      src={listFeatureImage[Title]}
-                      alt="Probiotics"
-                      style={{
-                        objectFit: "fill",
-                      }}
-                    />
+      <Box
+        sx={{ display: "flex", flexDirection: "row", justifyContent: "center" }}
+      >
+        <Box
+          sx={{
+            width: isDeskopScreen ? "70vw" : "100%",
+            height: isDeskopScreen ? "60vh" : "100%",
+            backgroundColor: "#103f4e",
+            borderRadius: "10px",
+          }}
+        >
+          <Grid container>
+            {Features.map((el, index) => {
+              const { Title, DescriptionOne, DescriptionTwo } = el;
+              return (
+                <Grid key={index} size={{ md: 4, sm: 4, xs: 12 }}>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      flexDirection: "row",
+                      justifyContent: "center",
+                      marginTop: "-54px",
+                      marginBottom: "100px",
+                    }}
+                  >
+                    <Stack direction="column">
+                      <Box>
+                        <Image
+                          src={listFeatureImage[Title]}
+                          alt={Title}
+                          style={{
+                            objectFit: "fill",
+                          }}
+                          loading="lazy"
+                        />
+                      </Box>
+                      <Box component="h3" sx={{ color: "#fff" }}>
+                        {Title}
+                      </Box>
+                      <Box component="h6" sx={{ color: "#c4d2d6" }}>
+                        {DescriptionOne}
+                      </Box>
+                      <Box component="h6" sx={{ color: "#c4d2d6" }}>
+                        {DescriptionTwo}
+                      </Box>
+                    </Stack>
                   </Box>
-                  <Box component="h3">{Title}</Box>
-                  <Box component="h6" sx={{ color: "#c4d2d6" }}>
-                    {DescriptionOne}
-                  </Box>
-                  <Box component="h6" sx={{ color: "#c4d2d6" }}>
-                    {DescriptionTwo}
-                  </Box>
-                </Stack>
-              </Box>
-            </Grid>
-          );
-        })}
-
-        <Grid size={{ md: 2, sm: 2, xs: 12 }}></Grid>
-      </Grid>
-      <Grid container>
-        <Grid size={{ md: 2, sm: 2, xs: 12 }}></Grid>
-
-        {FeaturePartTwo.map((el) => {
-          const { Title, DescriptionOne, DescriptionTwo } = el;
-          return (
-            <Grid
-              size={{ md: 3, sm: 3, xs: 12 }}
-              sx={{ backgroundColor: "#103f4e", color: "#fff" }}
-            >
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "row",
-                  justifyContent: "center",
-                  padding: "20px",
-                }}
-              >
-                <Stack direction="column">
-                  <Box>
-                    <Image
-                      src={listFeatureImage[Title]}
-                      alt="Probiotics"
-                      style={{
-                        objectFit: "fill",
-                      }}
-                    />
-                  </Box>
-                  <Box component="h3">{Title}</Box>
-                  <Box component="h6" sx={{ color: "#c4d2d6" }}>
-                    {DescriptionOne}
-                  </Box>
-                  <Box component="h6" sx={{ color: "#c4d2d6" }}>
-                    {DescriptionTwo}
-                  </Box>
-                </Stack>
-              </Box>
-            </Grid>
-          );
-        })}
-
-        <Grid size={{ md: 2, sm: 2, xs: 12 }}></Grid>
-      </Grid>
+                </Grid>
+              );
+            })}
+          </Grid>
+        </Box>
+      </Box>
     </>
   );
 }
