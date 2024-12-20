7a6aaff853c88ae7b5affba951e3298b43c6720f
Balaji <81224567+balaji1610@users.noreply.github.com>
2024-12-20 19:05:43 +0530

    Fixed HeroSection

--- src/app/components/heroSection.tsx
+++ src/app/components/heroSection.tsx
@@ -33,145 +33,158 @@ export default function HeroSection() {
   return (
     <>
       <Logo />
-      <Grid
-        container
+
+      <Box
         sx={{
           backgroundColor: "#e3f5fb",
           margin: isDeskopScreen ? "3vh" : "2vh 2vh 15vh 2vh",
           height: isDeskopScreen ? "110vh" : "auto",
         }}
       >
-        <Grid size={{ md: 12, sm: 12, xs: 12 }}>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "center",
-              color: "#003269",
-              marginTop: "5vh",
-              height: isDeskopScreen ? "2vh" : "0",
-            }}
-          >
-            <Typography variant={isDeskopScreen ? "h1" : "h4"} gutterBottom>
-              {Heading}
-            </Typography>
-          </Box>
-        </Grid>
-
-        <Grid container size={{ md: 12, sm: 12, xs: 12 }}>
-          <Grid size={{ md: 1, sm: 2, xs: 12 }}></Grid>
-          <Grid
-            size={{ md: 3, sm: 2, xs: 12 }}
-            sx={{ display: "flex", flexDirection: "row", alignItems: "center" }}
-          >
+        <Grid container>
+          <Grid size={{ md: 12, sm: 12, xs: 12, xl: 12 }}>
             <Box
               sx={{
                 display: "flex",
                 flexDirection: "row",
                 justifyContent: "center",
+                color: "#003269",
+                marginTop: "5vh",
+                height: isDeskopScreen ? "2vh" : "0",
               }}
             >
-              <Stack>
-                <Box sx={{ color: "#c4d2d6" }}>
-                  {" "}
-                  <Typography
-                    variant={isDeskopScreen ? "h5" : "h4"}
-                    gutterBottom
-                  >
-                    {LeftHeading}
-                  </Typography>
-                </Box>
-                <Box sx={{ color: "#103f4e" }}>
-                  {" "}
-                  <Typography
-                    variant={isDeskopScreen ? "h4" : "h4"}
-                    gutterBottom
-                  >
-                    {SubHeading}
-                  </Typography>
-                </Box>
+              <Typography variant={isDeskopScreen ? "h1" : "h4"} gutterBottom>
+                {Heading}
+              </Typography>
+            </Box>
+          </Grid>
+        </Grid>
+
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            marginTop: isDeskopScreen ? "20vh" : "0",
+          }}
+        >
+          <Box sx={{ width: "80%" }}>
+            <Grid container spacing={8}>
+              <Grid
+                size={{ md: 4, sm: 4, xs: 12, xl: 4 }}
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
                 <Box
                   sx={{
-                    padding: "10px",
-                    backgroundColor: "#103f4e",
-                    width: "100px",
-                    color: " #fff",
-                    borderRadius: "14px",
-                    cursor: "pointer",
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "center",
                   }}
                 >
-                  <Typography variant="caption" gutterBottom>
-                    {Button}
-                  </Typography>
-                </Box>
-              </Stack>
-            </Box>
-          </Grid>
-          <Grid size={{ md: 4, sm: 4, xs: 12 }}>
-            <Box>
-              <Image
-                src={Probiotics}
-                alt="Probiotics"
-                style={{
-                  objectFit: "fill",
-                  height: "50vh",
-                }}
-              />
-            </Box>
-          </Grid>
-          <Grid size={{ md: 4, sm: 4, xs: 12 }} sx={{}}>
-            {RigthSideContent.map((el, index) => {
-              const { Title, DescriptionOne, DescriptionTwo } = el;
-              return (
-                <Box key={index} sx={{ marginTop: "15px" }}>
-                  <Stack direction="row" spacing={2}>
-                    <Box>
-                      <Image
-                        src={listImages[Title]}
-                        alt="Title"
-                        style={{
-                          objectFit: "fill",
-                        }}
-                      />
+                  <Stack>
+                    <Box sx={{ color: "#6c797c" }}>
+                      {" "}
+                      <Typography
+                        variant={isDeskopScreen ? "h5" : "h4"}
+                        gutterBottom
+                      >
+                        {LeftHeading}
+                      </Typography>
                     </Box>
-                    <Stack direction="column">
-                      <Box sx={{ color: "#103f4e", fontWeight: "bold" }}>
-                        <Typography variant="subtitle1">{Title}</Typography>
-                      </Box>
-                      <Box sx={{ color: "#c4d2d6" }}>
-                        <Typography variant="subtitle2">
-                          {DescriptionOne}
-                        </Typography>
-                        <Typography variant="subtitle2">
-                          {DescriptionTwo}
-                        </Typography>
-                      </Box>
-                    </Stack>
+                    <Box sx={{ color: "#103f4e" }}>
+                      {" "}
+                      <Typography
+                        variant={isDeskopScreen ? "h4" : "h4"}
+                        gutterBottom
+                      >
+                        {SubHeading}
+                      </Typography>
+                    </Box>
+                    <Box
+                      sx={{
+                        padding: "10px",
+                        backgroundColor: "#103f4e",
+                        width: "100px",
+                        color: " #fff",
+                        borderRadius: "14px",
+                        cursor: "pointer",
+                      }}
+                    >
+                      <Typography variant="caption" gutterBottom>
+                        {Button}
+                      </Typography>
+                    </Box>
                   </Stack>
                 </Box>
-              );
-            })}
-          </Grid>
-          <Grid size={{ md: 1, sm: 2, xs: 12 }}></Grid>
-        </Grid>
-        <Grid size={{ md: 4, sm: 4, xs: 12 }}>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "center",
-            }}
-          >
-            <Image
-              src={TwoDot}
-              alt="TwoDot"
-              style={{
-                objectFit: "fill",
-              }}
-            />
+              </Grid>
+              <Grid size={{ md: 4, sm: 4, xs: 12, xl: 4 }}>
+                <Box sx={{ marginTop: "-6vh" }}>
+                  <Image
+                    src={Probiotics}
+                    alt="Probiotics"
+                    style={{
+                      objectFit: "fill",
+                    }}
+                  />
+                </Box>
+              </Grid>
+              <Grid size={{ md: 4, sm: 4, xs: 12, xl: 4 }}>
+                {RigthSideContent.map((el, index) => {
+                  const { Title, DescriptionOne, DescriptionTwo } = el;
+                  return (
+                    <Box key={index} sx={{ marginTop: "15px" }}>
+                      <Stack direction="row" spacing={2}>
+                        <Box>
+                          <Image
+                            src={listImages[Title]}
+                            alt="Title"
+                            style={{
+                              objectFit: "fill",
+                            }}
+                          />
+                        </Box>
+                        <Stack direction="column">
+                          <Box sx={{ color: "#1b6780", fontWeight: "bold" }}>
+                            <Typography variant="h6">{Title}</Typography>
+                          </Box>
+                          <Box sx={{ color: "#6c797c" }}>
+                            <Typography variant="subtitle2">
+                              {DescriptionOne}
+                            </Typography>
+                            <Typography variant="subtitle2">
+                              {DescriptionTwo}
+                            </Typography>
+                          </Box>
+                        </Stack>
+                      </Stack>
+                    </Box>
+                  );
+                })}
+              </Grid>
+            </Grid>
           </Box>
-        </Grid>
-      </Grid>
+        </Box>
+
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "flex-start",
+          }}
+        >
+          <Image
+            src={TwoDot}
+            alt="TwoDot"
+            style={{
+              objectFit: "fill",
+            }}
+          />
+        </Box>
+      </Box>
     </>
   );
 }
